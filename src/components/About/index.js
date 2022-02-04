4d88e745ec32898887e1a7bf4a6772abc9373a18
Melissa Tan <melissatan9683@gmail.com>
2022-02-03 22:18:52 -0800

    about me text completed

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,10 +1,19 @@
-import React from 'react'
-import "./style.css"
+import React from "react";
+import "./style.css";
 
 export default function About() {
-    return (
-        <div className="about" id="about">
-            About Me
-        </div>
-    )
+  return (
+    <div className="about" id="about">
+      <h3>Making people's lives easier</h3>
+      <p>
+        As a full stack software engineer with 6 months of working experience in
+        addition to 6 months of formal education through an intensive program,
+        my aim is to create easy to use and engaging web applications to allow
+        enterprises to focus on what is important. Making information readily
+        accessible is imperative to saving time. I take pride in designing and
+        implementing sustainable solutions in which users can interface with in
+        a seamless manner.
+      </p>
+    </div>
+  );
 }
